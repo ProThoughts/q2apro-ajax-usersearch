b80a704d84615785b617013eb500d0726746eb19
q2apro <info@q2apro.com>
2015-04-10 21:08:28 +0300

    initial commit

---
+++ q2apro-ajax-usersearch-lang-default.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+	Plugin Name: q2apro-ajax-usersearch
+	Plugin URI: http://www.q2apro.com/plugins/usersearch
+	Plugin Description: Extend the users page with an ajax searchfield, also provides a user search widget.
+*/
+
+	return array(
+		// default
+		'enable_plugin' => 'Enable Plugin',
+		'minimum_level' => 'Level to access this page and edit posts:',
+		'plugin_disabled' => 'Plugin has been disabled.',
+		'access_forbidden' => 'Access forbidden.',
+		'plugin_page_url' => 'Open page in forum:',
+		'contact' => 'For questions please visit ^1q2apro.com^2',
+		
+		// plugin specific
+		'input_placeholder' => 'Username',
+		'searchlabel' => 'Type to find users:',
+		'no_user_found' => 'No user found.',
+		'admin_imgsize' => 'Image size of Avatars:',
+		'admin_maxshow' => 'Maximal users to show when searching:',
+	);
+	
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
